258fc9044f9f15bbdaaae9feb57ccbe9d09701fe
taroxd <taroxd@outlook.com>
2017-04-17 14:31:32 +0800

    remove unused method

--- LEProc/Program.cs
+++ LEProc/Program.cs
@@ -39,7 +39,6 @@ namespace LEProc
             if (args.Length == 0)
             {
                 MessageBox.Show("Usage: LEProc.exe path [args]", "LEProc");
-
                 return;
             }
 
@@ -75,7 +74,7 @@ namespace LEProc
                 AnsiCodePage = (uint)textInfo.ANSICodePage,
                 OemCodePage = (uint)textInfo.OEMCodePage,
                 LocaleID = (uint)textInfo.LCID,
-                DefaultCharset = (uint)GetCharsetFromANSICodepage(textInfo.ANSICodePage),
+                DefaultCharset = 128,
                 HookUILanguageAPI = 0,
                 Timezone = TIMEZONE,
                 NumberOfRegistryRedirectionEntries = registries?.Length ?? 0,
@@ -101,96 +100,28 @@ namespace LEProc
             }
         }
 
-        private static int GetCharsetFromANSICodepage(int ansicp)
-        {
-            const int ANSI_CHARSET = 0;
-            const int DEFAULT_CHARSET = 1;
-            const int SYMBOL_CHARSET = 2;
-            const int SHIFTJIS_CHARSET = 128;
-            const int HANGEUL_CHARSET = 129;
-            const int HANGUL_CHARSET = 129;
-            const int GB2312_CHARSET = 134;
-            const int CHINESEBIG5_CHARSET = 136;
-            const int OEM_CHARSET = 255;
-            const int JOHAB_CHARSET = 130;
-            const int HEBREW_CHARSET = 177;
-            const int ARABIC_CHARSET = 178;
-            const int GREEK_CHARSET = 161;
-            const int TURKISH_CHARSET = 162;
-            const int VIETNAMESE_CHARSET = 163;
-            const int THAI_CHARSET = 222;
-            const int EASTEUROPE_CHARSET = 238;
-            const int RUSSIAN_CHARSET = 204;
-            const int MAC_CHARSET = 77;
-            const int BALTIC_CHARSET = 186;
-
-            var charset = ANSI_CHARSET;
-
-            switch (ansicp)
-            {
-                case 932: // Japanese
-                    charset = SHIFTJIS_CHARSET;
-                    break;
-                case 936: // Simplified Chinese
-                    charset = GB2312_CHARSET;
-                    break;
-                case 949: // Korean
-                    charset = HANGEUL_CHARSET;
-                    break;
-                case 950: // Traditional Chinese
-                    charset = CHINESEBIG5_CHARSET;
-                    break;
-                case 1250: // Eastern Europe
-                    charset = EASTEUROPE_CHARSET;
-                    break;
-                case 1251: // Russian
-                    charset = RUSSIAN_CHARSET;
-                    break;
-                case 1252: // Western European Languages
-                    charset = ANSI_CHARSET;
-                    break;
-                case 1253: // Greek
-                    charset = GREEK_CHARSET;
-                    break;
-                case 1254: // Turkish
-                    charset = TURKISH_CHARSET;
-                    break;
-                case 1255: // Hebrew
-                    charset = HEBREW_CHARSET;
-                    break;
-                case 1256: // Arabic
-                    charset = ARABIC_CHARSET;
-                    break;
-                case 1257: // Baltic
-                    charset = BALTIC_CHARSET;
-                    break;
-            }
-
-            return charset;
-        }
-
         public static bool CheckCoreDLLs()
         {
             string[] dlls = { "LoaderDll.dll", "LocaleEmulator.dll" };
             return dlls.All(dll => File.Exists(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), dll)));
         }
 
-        public static string EnsureValidPath(string filePath)
+        public static string EnsureValidPath(string path)
         {
-            if (!String.Equals(Path.GetExtension(filePath), ".exe", StringComparison.OrdinalIgnoreCase))
+            if (!String.Equals(Path.GetExtension(path), ".exe", StringComparison.OrdinalIgnoreCase))
             {
-                filePath += ".exe";
+                path += ".exe";
             }
 
-            if (File.Exists(filePath))
+            if (File.Exists(path))
             {
-                return Path.GetFullPath(filePath);
+                return Path.GetFullPath(path);
             }
 
             var envPath = Environment.GetEnvironmentVariable("PATH");
-            foreach (var path in envPath.Split(';'))
+            foreach (var envPathEntry in envPath.Split(';'))
             {
-                var fullPath = Path.Combine(path, filePath);
+                var fullPath = Path.Combine(envPathEntry, path);
                 if (File.Exists(fullPath))
                 {
                     return fullPath;
